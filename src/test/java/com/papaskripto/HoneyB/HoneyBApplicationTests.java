d62e1c70703bec52b5793c9e895bde89bb2b1e18
papaskripto <bygeoffreyouma@gmail.com>
2026-03-10 13:55:13 +0300

    add starter files

---
+++ src/test/java/com/papaskripto/HoneyB/HoneyBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaskripto.HoneyB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
